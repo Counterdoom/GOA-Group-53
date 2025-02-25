f89fa3d0e1fa7a97f1646f62f7ed79921f51c2d1
Counterdoom <davidmanguashvili@gmail.com>
2025-02-25 17:34:26 +0400

    Create cw06b.py

---
+++ Level 06/cw/cw06b.py
@@ -0,0 +1,9 @@
+father_age = 40
+mother_age = 39
+
+father_future_age = father_age + 20
+mother_future_age = mother_age + 20
+
+print("მშობლებიც 20 წელში:")
+print(father_future_age)
+print(mother_future_age)
