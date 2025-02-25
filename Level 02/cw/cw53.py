6d6823cf6ba1cf1348896c26930550ae7d421a61
Counterdoom <davidmanguashvili@gmail.com>
2025-02-25 15:57:45 +0400

    Create cw53.py

---
+++ Level 02/cw/cw53.py
@@ -0,0 +1,14 @@
+#01
+name = "დავით"
+surname = "მანგუაშვილი"
+age = 14
+
+print(name)
+print(surname)
+print(age)
+
+#02
+hobby = "კალათბურთი"
+car = "Toyota"
+
+print(name, surname, age, hobby, car)
