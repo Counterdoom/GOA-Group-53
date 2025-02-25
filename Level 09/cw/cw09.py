2b701e46e0ec313ebe2f610c2656ae0d761f1f92
Counterdoom <davidmanguashvili@gmail.com>
2025-02-25 20:57:01 +0400

    Create cw09.py

---
+++ Level 09/cw/cw09.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ka">
+<head>
+    <title>GOA</title>
+</head>
+<body>
+    <h1><u>GOA - General Overview</u></h1>
+    <p><b>GOA</b> არის ერთ-ერთი წამყვანი სასწავლო პლატფორმა, რომელიც ხელს უწყობს განათლების მიღებას ინოვაციური მეთოდებით.</p>
+    <p><i>GOA-ში ისწავლები Python, CSS, HTML, Javascript</i></p>
+    <img src="./goa2.png" alt="GOA Logo" width="300">
+</body>
+</html>
