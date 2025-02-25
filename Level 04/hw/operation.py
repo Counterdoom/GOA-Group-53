b987a504a647a01b0770de1ef0ec2191fbefcab6
Counterdoom <davidmanguashvili@gmail.com>
2025-02-25 17:07:47 +0400

    Create operation.py

---
+++ Level 04/hw/operation.py
@@ -0,0 +1,29 @@
+#+
+print(3 + 3)
+print(7.5 + 7.2)
+print(1 + (-1))
+
+#-
+print(100 - 0.1)
+print(20 - 16)
+print(30 - 31)
+
+#*
+print(4 * 2)
+print(8 * 7)
+print(11 * 11)
+
+#/
+print(4 / 8)
+print(100/ 25)
+print(6 / 7)
+
+#**
+print(4 ** 4)
+print(10 ** 2)
+print(8 ** 3)
+
+#%
+print(10 % 2)
+print(97 % 12)
+print(56 % 55)
