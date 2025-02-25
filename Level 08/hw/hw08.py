c1b0f09fc6d9edee8d54ac95d6cef3311958d6a0
Counterdoom <davidmanguashvili@gmail.com>
2025-02-25 19:28:02 +0400

    Create hw08.py

---
+++ Level 08/hw/hw08.py
@@ -0,0 +1,14 @@
+#01
+# Input - მონაცემის შეტანა
+# Output - გამოტანა 
+
+#02
+name = input("საბა")
+output(name)
+
+#03
+name2 = input(გიორგი)
+surname = input(ჭავჭავძე)
+age = input(60)
+
+print("გამარჯობა, მე ვარ " + name2 + "" + surname + " და ვარ " + age + "წლის")
