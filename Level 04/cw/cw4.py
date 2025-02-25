3d466ff0029bd8e731679fc91f8a9c529d70b7f1
Counterdoom <davidmanguashvili@gmail.com>
2025-02-25 17:08:32 +0400

    Create cw4.py

---
+++ Level 04/cw/cw4.py
@@ -0,0 +1,22 @@
+# String
+str1 = "გამარჯობა"
+str2 = "პროგრამირება"
+str3 = "Python"
+str4 = "ჩემი პროექტი"
+
+# Integer
+int1 = 5
+int2 = 10
+int3 = 15
+int4 = 20
+
+# Float
+float1 = 3.14
+float2 = 2.718
+float3 = 1.618
+float4 = 0.577
+
+# Print
+print(str1, str2, str3, str4)
+print(int1, int2, int3, int4)
+print(float1, float2, float3, float4)
