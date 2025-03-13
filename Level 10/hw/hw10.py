5fbba406bd52e3ea29cad3ccc774a557793f02d3
Counterdoom <davidmanguashvili@gmail.com>
2025-03-13 23:26:52 +0400

    Create hw10.py

---
+++ Level 10/hw/hw10.py
@@ -0,0 +1,61 @@
+#01
+name = input("დავით")
+age = input(14)
+
+print("გამარჯობა,", name + "! შენ ხარ", age, "წლის.")
+
+#02
+mom_age = int(input(39))
+dad_age = int(input(40))
+bro_age = int(input(8))
+sis_age = int(input(500))
+
+print("დედა 10 წელიწადში იქნება", mom_age + 10, "წლის")
+print("მამა 10 წელიწადში იქნება", dad_age + 10, "წლის")
+print("ძმა 10 წელიწადში იქნება", bro_age + 10, "წლის")
+print("და 10 წელიწადში იქნება", sis_age + 10, "წლის")
+
+#03
+num1 = int(input(75))
+num2 = int(input(86))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+#04
+num1 = 10
+num2 = 5
+num3 = 3
+num4 = 8
+num5 = 2
+
+print(num1 + num2)
+print(num3 - num4)
+print(num4 * num5)
+print(num1 / num3)
+print(num2 ** num5)
+print(num4 % num3)
+
+print((num1 + num2) / num3)
+print((num4 - num5) * num1)
+print(num2 * num3 + num4)
+print(num1 ** num5 % num3)
+
+#Boss
+name = input("საბა")
+surname = input("ჭავჭავაძე")
+age = input(74)
+email = input("saba001@gmail.com")
+birth_year = input(1951)
+birthday = input("04/16")
+
+print("რეგისტრაცია დასრულებულია!")
+print("სახელი:", name)
+print("გვარი:", surname)
+print("ასაკი:", age)
+print("ელფოსტა:", email)
+print("დაბადების წელი:", birth_year)
+print("დაბადების თარიღი:", birthday)
+
