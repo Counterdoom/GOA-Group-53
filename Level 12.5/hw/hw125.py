e481cd4d2acc8c637028ef11fc5f845fafd7c83a
Counterdoom <davidmanguashvili@gmail.com>
2025-03-14 11:39:59 +0400

    Create hw125.py

---
+++ Level 12.5/hw/hw125.py
@@ -0,0 +1,92 @@
+#01
+print(5 > 3)
+print(10 > 9)
+print(7 > 4)
+print(100 > 50)
+print(9 > 6)
+
+print(2 < 5)
+print(1 < 10)
+print(8 < 9)
+print(0 < 3)
+print(4 < 7)
+
+print(5 >= 5)
+print(10 >= 5)
+print(7 >= 7)
+print(9 >= 3)
+print(4 >= 4)
+
+print(5 <= 5)
+print(1 <= 10)
+print(8 <= 9)
+print(0 <= 3)
+print(4 <= 7)
+
+print(5 == 5)
+print(10 == 10)
+print(7 == 7)
+print(9 == 9)
+print(4 == 4)
+
+print(5 != 3)
+print(10 != 9)
+print(7 != 4)
+print(100 != 50)
+print(9 != 6)
+
+#02
+# +
+print(5 + 3)
+print(10 + 9)
+print(7 + 4)
+print(100 + 50)
+print(9 + 6)
+
+# -
+print(5 - 3)
+print(10 - 9)
+print(7 - 4)
+print(100 - 50)
+print(9 - 6)
+
+# *
+print(5 * 3)
+print(10 * 9)
+print(7 * 4)
+print(100 * 50)
+print(9 * 6)
+
+# /
+print(10 / 5)
+print(20 / 4)
+print(50 / 10)
+print(100 / 2)
+print(9 / 3)
+
+# **
+print(2 ** 3)
+print(3 ** 2)
+print(4 ** 4)
+print(5 ** 3)
+print(6 ** 2)
+
+# %
+print(10 % 3)
+print(15 % 4)
+print(20 % 6)
+print(50 % 7)
+print(100 % 9)
+
+#03
+age = int(27)
+print("40 წელში იქნები", age + 40)
+
+#04
+num1 = int(4)
+num2 = int(7)
+
+print("ჯამი:", num1 + num2)
+print("სხვაობა:", num1 - num2)
+print("ნამრავლი:", num1 * num2)
+print("განაყოფი:", num1 / num2)
