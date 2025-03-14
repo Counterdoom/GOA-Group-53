4e1e99ba6e4d72d2489f088b656203fddac25817
Counterdoom <davidmanguashvili@gmail.com>
2025-03-14 11:45:37 +0400

    Create cw12.py

---
+++ Level 12.5/cw/cw12.py
@@ -0,0 +1,30 @@
+#01
+num1 = int(17)
+num2 = int(20)
+
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+print(num1 == num2)
+print(num1 != num2)
+
+#02
+width = int(25)
+height = int(64)
+
+perimeter = 2 * (width + height)
+print("პერიმეტრი არის:", perimeter)
+
+#03
+name = "დავითი"
+num = int(1000)
+
+print(name * num)
+
+#04
+birthyear = int(2010)
+date = int(2025)
+
+age = date - birthyear
+print("თქვენი ასაკი:", age)
