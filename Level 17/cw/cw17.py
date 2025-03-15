55702249912141988fc84b4426e4cf95aa3b9262
Counterdoom <davidmanguashvili@gmail.com>
2025-03-15 20:49:59 +0400

    Create cw17.py

---
+++ Level 17/cw/cw17.py
@@ -0,0 +1,16 @@
+#01
+for i in range(950, 1001):
+    if i % 2 == 0:
+        print(i)
+
+#02
+for i in range(100):
+    print("დავითი")
+
+#03
+for i in range(1000):
+    print("Goa Is the best")
+
+#04
+for i in range(100, 201):
+    print(i)
