b60b173e9467f87363ca4cb8f5940d55027b76b5
Counterdoom <davidmanguashvili@gmail.com>
2025-03-27 15:37:32 +0400

    Create cw21.py

---
+++ Level 21/cw/cw21.py
@@ -0,0 +1,12 @@
+#01
+cars = ["BMW", "Mercedes", "Toyota", "Tesla", "Ford"]
+print(cars)
+
+#02
+foods = ["Pizza", "Fries", "Burger", "Khinkali", "Mtsvadi"]
+print(foods)
+
+#03
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(num[2:8])
+print(num[5:10])
