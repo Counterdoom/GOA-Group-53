cd0632a71fc294e68ceb1af141291306d1688a79
Counterdoom <davidmanguashvili@gmail.com>
2025-04-13 22:59:03 +0400

    Create cw24.py

---
+++ Level 24/cw/cw24.py
@@ -0,0 +1,21 @@
+#01
+for i in range(21):
+    print(i)
+
+#02
+for i in range(21):
+    if i == 15:
+        continue
+    print(i)
+
+#03
+while True:
+  print("უსასრულო ციკლი")
+
+#04
+i = 0
+while True:
+    print(i)
+    i += 1
+    if i == 14:
+        break
