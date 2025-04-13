1e7138eb20e461de9a162a9b9f07004dba7b81bc
Counterdoom <davidmanguashvili@gmail.com>
2025-04-13 23:11:01 +0400

    Update and rename hw23.html to hw23.py

---
+++ Level 23/hw/hw23.py
@@ -0,0 +1,22 @@
+#01
+my_list = ["phone", "laptop", "book", "pen", "bottle", "notebook", "charger", "key", "wallet", "watch"]
+print(my_list)
+
+#02
+list = [17, 2, 3, 4, 10, 6, 100, 8, 9, 11]
+list.sort()
+print(list)
+
+#03
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(nums[3:8])
+
+#04
+print(nums[-3:])
+
+#05
+my_list = ["apple", "banana", "cherry"]
+
+print(my_list[0])
+print(my_list[1])
+
