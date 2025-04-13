4d2ee082affb207a6a0911125b4a8cabfd760f43
Counterdoom <davidmanguashvili@gmail.com>
2025-04-13 23:19:02 +0400

    Create hw25.py

---
+++ Level 25/hw/hw25.py
@@ -0,0 +1,22 @@
+#01
+# def ფუნქცია არის ფუნქციის განსაზღვრა. 
+# ფუნქცია გამოიყენება კოდის მრავალჯერადად გამოყენებისთვის. 
+# სტრუქტურა:
+# def ფუნქციის_სახელი(პარამეტრები):
+#     კოდი
+
+#02
+def combine_texts(text1, text2):
+    return text1 + text2
+
+#03
+def repeat_text(text, number):
+    return text * number
+
+#04
+def greet(name):
+    return f"გამარჯობა, {name}!"
+
+#05
+def format_words(words):
+    return f"{words[0]}, {words[1]} და {words[2]}"
