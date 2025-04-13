429e9db663c115a8283de64661f4b6bfef19c1cd
Counterdoom <davidmanguashvili@gmail.com>
2025-04-13 23:09:14 +0400

    Update and rename cw23.html to cw23.py

---
+++ Level 23/cw/cw23.py
@@ -0,0 +1,20 @@
+#01
+numbers = [5, 10, 15, 20, 25]
+
+print("პირველი ელემენტი:", numbers[0])
+print("ბოლო ელემენტი:", numbers[-1])
+print("შუა ელემენტი:", numbers[len(numbers) // 2])
+
+#02
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(num)
+
+#03
+fruits = ["banana", "orange"]
+fruits.append("apple")
+print(fruits)
+
+#04
+animals = ["cat", "dog", "lion", "tiger"]
+index = animals.index("dog")
+print("Dog-ის ინდექსი:", index)
