2f03653ef4e6e0c7f9069eb110b6c4cd0648f270
Counterdoom <davidmanguashvili@gmail.com>
2025-04-29 23:02:35 +0400

    Create hw27.py

---
+++ Level 27/hw/hw27.py
@@ -0,0 +1,65 @@
+# 1
+def to_upper(text):
+    return text.upper()
+
+# 2
+name = ("david")
+upper_case = name.capitalize()
+
+# 3
+sentence = "ეს არის ძალიან ლამაზი ქალაქი"
+search_word = sentence.find("ლამაზი")
+
+# 4
+def mix_case(a, b):
+    return a.upper() + b.lower()
+
+# 5
+# upper() მაგალითები
+u1 = "python".upper()
+u2 = "hello world".upper()
+u3 = "123abc".upper()
+u4 = "PyThOn".upper()
+u5 = "test@123".upper()
+
+# lower() მაგალითები
+l1 = "PYTHON".lower()
+l2 = "HeLLo WoRLD".lower()
+l3 = "123ABC".lower()
+l4 = "PY@TH@ON".lower()
+l5 = "MIXEDcase".lower()
+
+# replace() მაგალითები
+r1 = "apple".replace("a", "o")
+r2 = "banana".replace("na", "ma")
+r3 = "123123".replace("1", "9")
+r4 = "hello world".replace("world", "python")
+r5 = "test test".replace("test", "pass")
+
+# capitalize() მაგალითები
+c1 = "python".capitalize()
+c2 = "hello WORLD".capitalize()
+c3 = "123abc".capitalize()
+c4 = "tESTing".capitalize()
+c5 = "capitalize me".capitalize()
+
+# swapcase() მაგალითები
+s1 = "Python".swapcase()
+s2 = "HeLLo WoRLD".swapcase()
+s3 = "123aBc".swapcase()
+s4 = "SwapCASE".swapcase()
+s5 = "EXample".swapcase()
+
+# find() მაგალითები
+f1 = "hello world".find("world")
+f2 = "banana".find("n")
+f3 = "testing function".find("func")
+f4 = "abcdef".find("d")
+f5 = "look here".find("not")
+
+# capitalize() მაგალითები
+c1 = "python".capitalize()
+c2 = "hello WORLD".capitalize()
+c3 = "123abc".capitalize()
+c4 = "tESTing".capitalize()
+c5 = "capitalize me".capitalize()
