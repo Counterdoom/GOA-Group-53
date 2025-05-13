a42eec1561391a5e19ab4323e98d431fee5db268
Counterdoom <davidmanguashvili@gmail.com>
2025-05-13 21:17:40 +0400

    Create cw29.py

---
+++ Level 29/cw/cw29.py
@@ -0,0 +1,14 @@
+#01
+def upp(s):
+    for letter in s:
+        print(letter.upper())
+    
+print(upp("giorgi"))
+
+#02
+def eleme(lst):
+    for i in lst:
+        print(i)
+        
+my_list = [1, 2, 3, 4, 5]
+eleme(my_list)
